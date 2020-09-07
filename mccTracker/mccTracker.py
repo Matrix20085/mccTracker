9dcb2e37c2877e2721e195a53ffc51b9b7d802dd
Matrix20085 <mikemaster2000btfu@yahoo.com>
2020-09-07 14:03:04 -0500

    Initial code commit

---
+++ mccTracker/mccTracker.py
@@ -0,0 +1,39 @@
+import re
+import steamAPI
+import configparser
+
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from flask import Flask, render_template, flash
+from wtforms.validators import DataRequired
+
+class usernameForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+
+parser = configparser.ConfigParser()
+parser.read('config.ini')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = parser.get('keys','SECRET_KEY')
+
+@app.route('/', methods=['GET','POST'])
+@app.route('/home', methods=['GET','POST'])
+def home():
+    form = usernameForm()
+    if form.username.data:
+        steamID = form.username.data
+        if re.search(r"^\d{17}$",steamID):
+            flash(f'Acount loaded!', 'success')
+            return render_template('master.html', form=form, title=form.username.data, userData= steamAPI.getUserData(steamID).values())
+        else:
+            steamID = steamAPI.getSteamID(steamID)
+            if steamID != 0:
+                flash(f'Acount loaded!', 'success')
+                return render_template('master.html', form=form, title=form.username.data, userData= steamAPI.getUserData(steamID).values())
+            else:
+                flash(f'Acount not found. Only SteamIDs and account names cane be used.', 'danger')
+                return render_template('master.html', form=form)
+    else:
+        return render_template('master.html', form=form)
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ mccTracker/steamAPI.py
@@ -0,0 +1,60 @@
+import json
+import time
+import requests
+import configparser
+
+from datetime import datetime
+
+parser = configparser.ConfigParser()
+parser.read('config.ini')
+key = parser.get('keys','steamAPI')
+
+def getUserData(steamAccountID):
+    
+
+    #steamAccountID = parser.get('keys','testPlayer')
+
+    playerDataURL = "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=976730&key=" + key + "&steamid=" + steamAccountID
+    mccDataURL = "http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?appid=976730&key=" + key
+    achievementDataURL = "http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=976730"
+
+    playerData = json.loads(requests.get(playerDataURL).content)
+    mccData = json.loads(requests.get(mccDataURL).content)
+    achievementData = json.loads(requests.get(achievementDataURL).content)
+
+    #if (playerData.status_code != 200 and mccData.status_code != 200 and achievementData.status_code != 200):
+    #    return {"API issues"}
+
+    fullUserData = {}
+
+    for achievement in playerData['playerstats']['achievements']:
+        currentName = achievement['apiname']
+        fullUserData[currentName] = {}
+        fullUserData[currentName]['achieved'] = achievement['achieved']
+        if fullUserData[currentName]['achieved'] == 1:
+            fullUserData[currentName]['unlockTime'] = datetime.strftime(datetime.fromtimestamp(achievement['unlocktime']),"%b %d %Y @ %I:%M %p")
+        else:
+            fullUserData[currentName]['unlockTime'] = ""
+        for mccAchievement in mccData['game']['availableGameStats']['achievements']:
+            if mccAchievement['name'] == currentName:
+                fullUserData[currentName]['displayName'] = mccAchievement['displayName']
+                fullUserData[currentName]['description'] = mccAchievement['description']
+                if fullUserData[currentName]['achieved'] == 1:
+                    fullUserData[currentName]['icon'] = mccAchievement['icon']
+                else:
+                    fullUserData[currentName]['icon'] = mccAchievement['icongray']
+        for statsAchievement in achievementData['achievementpercentages']['achievements']:
+            if statsAchievement['name'] == currentName:
+                fullUserData[currentName]['percent'] = round(statsAchievement['percent'],1)
+
+    
+                
+    return fullUserData
+
+def getSteamID(userName):
+    steamIDURL = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=" + key + "&vanityurl=" + userName
+    steamID = json.loads(requests.get(steamIDURL).content)
+    if steamID['response']['success'] == 1:
+        return steamID['response']['steamid']
+    else:
+        return 0
